6024f576043d8cfc0f821b7c4286ffbb2b03cf84
RobertNeary16 <robertneary16@PHS221_4352.FG.local>
2015-12-02 11:00:45 -0500

    12/2//15
    Changed code for player 12

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -419,18 +419,20 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 12:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'Friendly Hate'
         else:
-            # use history, opponent_history, score, opponent_score
-            # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
+            if player == 15:
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
-            else:
-                return 'c' #otherwise collude
-    
-    
+            if player == 13:
+                return 'b'
+            if len(opponent_history)==0:
+                return 'c'
+                while opponent_history[-1]=='c':
+                    return 'c'
+                else:
+                    return 'b'
+                
+         
 
 
     ######
